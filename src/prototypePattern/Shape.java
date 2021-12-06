72fdc37bbe56b4ca99cc89f9bc437e56717b68cc
DaeHyeonHong <fholng@outlook.com>
2021-12-06 22:51:06 +0900

    [Design Pattern] Builder Pattern
    part.one

--- src/prototypePattern/Shape.java
+++ src/prototypePattern/Shape.java
@@ -4,12 +4,12 @@ public class Shape implements Cloneable {
 
     private String id;
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
 }
