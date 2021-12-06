91168f19e66cb7e9360119d9b6e9b7c72e8fdfbe
hongdae <fholng@outlook.com>
2021-12-06 21:49:28 +0900

    [Design Pattern] Prototype Pattern

---
+++ src/prototypePattern/Shape.java
@@ -0,0 +1,15 @@
+package prototypePattern;
+
+public class Shape implements Cloneable {
+
+    private String id;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}
